78020defb1e33b71904aceef393e602e3f0c3ae6
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-07-12 03:45:52 +0530

    add vault and escrow

---
+++ amm/programs/amm/src/instructions/mod.rs
@@ -0,0 +1,5 @@
+pub mod initialize;
+pub mod initialize::*;
+
+pub mod deposit;
+pub mod deposit::*;
---
+++ amm/programs/amm/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5qDMGCMziV5ZGrd7m1TZB4epxtV6KkCLjRCzzXub3vbV");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
