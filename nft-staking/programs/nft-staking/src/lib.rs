c332052ae6af284886506b903c03693dbc078a68
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-07-17 23:28:50 +0530

    Add nft-staking folder as regular directory

---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8X9ion7wCT5iJDtT7GEmEr6UrkJ6Ewi66tkJzVXXnMZZ");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
