54f015711abc718aab09621f5503c6eb0fbb0a17
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-08-30 22:31:00 +0530

    add solana-ico-dapp

---
+++ solana-ico-dapp/pages/_app.js
@@ -0,0 +1,34 @@
+import "../styles/globals.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+const network = WalletAdapterNetwork.Devnet;
+
+function MyApp({ Component, pageProps }) {
+  const endpoint = useMemo(() => clusterApiUrl(network, [network]));
+
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+  ], [network]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default MyApp;
